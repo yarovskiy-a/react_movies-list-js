51c80a25f2ed52e9ea282172414ff26d89b81d59
Yarovskiy Andrii <yarovskiy@gmail.com>
2026-07-04 12:00:05 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,12 @@
 
 import './App.scss';
 import moviesFromServer from './api/movies.json';
-
-const MovieCard = ({ film }) => (
-  <div className="card" data-cy="Movie">
-    <div className="card-image">
-      <figure className="image is-4by3">
-        <img data-cy="MovieImage" src={film.imgUrl} alt="Film logo" />
-      </figure>
-    </div>
-
-    <div className="card-content">
-      <div className="media">
-        <div className="media-left">
-          <figure className="image is-48x48">
-            <img src="images/imdb-logo.jpeg" alt="imdb" />
-          </figure>
-        </div>
-
-        <div className="media-content">
-          <p className="title is-8" data-cy="MovieTitle">
-            {film.title}
-          </p>
-        </div>
-      </div>
-
-      <div className="content">
-        <p data-cy="MovieDescription">{film.description}</p>
-
-        <a
-          href={`https://www.imdb.com/title/${film.imdbId}`}
-          data-cy="MovieLink"
-        >
-          IMDB
-        </a>
-      </div>
-    </div>
-  </div>
-);
-
-const MovieList = () => (
-  <div className="movies">
-    {moviesFromServer.map(film => (
-      <MovieCard film={film} key={film.imdbId} />
-    ))}
-  </div>
-);
+import { MovieList } from './components/MovieList/MovieList';
 
 export const App = () => (
   <div className="page">
     <div className="page-content">
-      <MovieList />
+      <MovieList movies={moviesFromServer} />
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
